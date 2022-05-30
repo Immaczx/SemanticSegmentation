83dda85a840fa065e1d126bae3dba923363ebeef
LAPTOP-3MGHFJE7\Camilo <cpelaezg@unal.edu.co>
2022-05-30 12:14:17 -0500

    Fixed model unet with mobilenet

--- models/UNetMobileNetV2.py
+++ models/UNetMobileNetV2.py
@@ -50,7 +50,7 @@ def get_decoder(skips):
         x = tf.keras.layers.Concatenate()([x,skip])
     return x
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-def get_model(output_channels=1,size=128,name="UNetMobileNetV2"):
+def get_model(output_channels=3,size=224,name="UNetMobileNetV2"):
     x = inputs = tf.keras.layers.Input(shape=[size,size,3])
 
     skips = get_encoder(input_shape=list(x.shape[1:]))(x)
