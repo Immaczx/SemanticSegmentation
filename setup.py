93af917a9cb977791db993f81eb510aeea032597
Camilo.pelaez <cpelaezg@unal.edu.co>
2023-04-23 21:43:30 -0500

    no message

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 import os
-from setuptools import setup, package_index
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+   README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='SemanticSegmentation',
     version='0.0.0',
-    packages= package_index(),
+    packages=['SemanticSegmentation'],
     author='Camilo Pelaez Garcia',
     author_email='cpelaezg@unal.edu.co',
     install_requires=['scikit-image',
@@ -18,4 +23,8 @@ setup(
     description="",
     zip_safe=False,
     python_requires='>=3.6',
+
+    long_description=README,
+    long_description_content_type='text/markdown',
+
 )
