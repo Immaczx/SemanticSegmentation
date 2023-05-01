eac77a4ee0782ee699575177beee5b85600e66d6
Camilo.pelaez <cpelaezg@unal.edu.co>
2023-05-01 10:47:22 -0500

    add ax cnfucion matrix plot

--- utils.py
+++ utils.py
@@ -67,51 +67,56 @@ def transform_image(Y):
 def plot_confusion_matrix(y_true, y_pred, classes,
                           normalize=True,
                           title=None,
-                          cmap=plt.cm.Blues):
-  """
-  This function prints and plots the confusion matrix.
-  Normalization can be applied by setting `normalize=True`.
-  """
-  if not title:
-      if normalize:
-          title = 'Normalized confusion matrix'
-      else:
-          title = 'Confusion matrix, without normalization'
+                          cmap=plt.cm.Blues,
+                          ax=None):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
 
-  # Compute confusion matrix
-  cm = confusion_matrix(y_true, y_pred)
-  # Only use the labels that appear in the data
-  #classes = classes[unique_labels(y_true, y_pred)]
-  if normalize:
-      cm = 100*cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-  
-  
-  fig, ax = plt.subplots()
-  im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
-  ax.figure.colorbar(im, ax=ax)
-  # We want to show all ticks...
-  ax.set(xticks=np.arange(cm.shape[1]),
-          yticks=np.arange(cm.shape[0]),
-          # ... and label them with the respective list entries
-          xticklabels=classes, yticklabels=classes,
-          title=title,
-          ylabel='True label',
-          xlabel='Predicted label')
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    #classes = classes[unique_labels(y_true, y_pred)]
+    if normalize:
+        cm = 100*cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
 
-  # Rotate the tick labels and set their alignment.
-  plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-            rotation_mode="anchor")
+    if ax is None:
+        fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+            yticks=np.arange(cm.shape[0]),
+            # ... and label them with the respective list entries
+            xticklabels=classes, yticklabels=classes,
+            title=title,
+            ylabel='True label',
+            xlabel='Predicted label')
 
-  # Loop over data dimensions and create text annotations.
-  fmt = '.1f' if normalize else 'd'
-  thresh = cm.max() / 2.
-  for i in range(cm.shape[0]):
-      for j in range(cm.shape[1]):
-          ax.text(j, i, format(cm[i, j], fmt),
-                  ha="center", va="center",
-                  color="white" if cm[i, j] > thresh else "black")
-  fig.tight_layout()
-  return ax
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+                rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.1f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+
+    if ax is None:
+        return ax
+    else:
+        return
 
 def dice_coef(y_true, y_pred):
     intersection = K.sum(y_true * y_pred, axis=[1,2,3])
